d7b9306ef6519dca1a7d01a677dc222320465bdf
xitman327 <mcxitos327@gmail.com>
2022-08-23 14:00:48 +0300

    asdasd

---
+++ include/defines.h
@@ -0,0 +1,48 @@
+#define board_name "XIT530 V4"
+
+#define in0 PJ0
+#define in1 PJ1
+#define in2 PJ2
+#define in3 PJ3
+#define in4 PJ4
+#define in5 PJ5
+#define in6 PJ6
+#define in7 PJ7
+#define in8 PC0
+#define in9 PC1
+#define in10 PC2
+#define in11 PC3
+#define in12 PC4
+#define in13 PC5
+#define in14 PC6
+#define in15 PC7
+
+#define ain_424_0 PF0
+#define ain_424_1 PF1
+#define ain_424_2 PF2
+#define ain_424_3 PF3
+#define ain0 PF4
+#define ain1 PF5
+#define ain2 PF6
+#define ain3 PF7
+
+#define out0 PH0
+#define out1 PH1
+#define out2 PH2
+#define out3 PH3
+#define out4 PH4
+#define out5 PH5
+#define out6 PH6
+#define out7 PH7
+#define out8 PB0
+#define out9 PB1
+#define out10 PB2
+#define out11 PB3
+#define out12 PB4
+#define out13 PB5
+#define out14 PB6
+#define out15 PB7
+
+#define onewire_pin PE2
+#define modbus_bus Serial2
+#define modbus_de PD4
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <ModbusMaster.h>
+#include <uRTCLib.h>
+
+#include "defines.h"
+
+void preTransmission()
+{
+  digitalWrite(modbus_de, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(modbus_de, 0);
+}
+ModbusMaster modbus;
+
+uRTCLib rtc(0x68);
+
+void setup() {
+  pinMode(modbus_de, OUTPUT);
+  modbus.begin(1, modbus_bus);
+  modbus.preTransmission(preTransmission);
+  modbus.postTransmission(postTransmission);
+
+  URTCLIB_WIRE.begin();
+  rtc.disable32KOut();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
